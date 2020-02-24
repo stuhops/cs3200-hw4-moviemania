2955050934c71c6f114c853048ae08e7ca8f91d4
stuhops <stuart.hopkins@aggiemail.usu.edu>
2020-02-23 22:15:44 -0700

    Got the basic info working

--- src/components/screens/info/movie_info.js
+++ src/components/screens/info/movie_info.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import { Col, Row} from 'react-native-easy-grid';
 import { Container, Text, Button } from 'native-base';
+import { Image, StyleSheet, } from 'react-native';
 
 export default class HomePage1 extends React.Component {
+
+  styles = StyleSheet.create({
+    imgThumbnail: {
+      width: 150,
+      height: 250,
+    },
+    movieTitle: {
+      fontWeight: 'bold',
+      fontSize: 36,
+      textAlign: 'center',
+      padding: 8,
+      marginBottom: 24,
+
+    },
+    movieInfo: {
+      fontSize: 16,
+    },
+    container: {
+      marginLeft: 24,
+      marginRight: 24,
+      marginBottom: 24,
+    },
+  })
+
+
   render() {
+    const { navigation, movie } = this.props.route.params;
+    console.log(movie);
     return (
       <Container>
-        <Row>
+        <Text style={this.styles.movieTitle}>{ movie.title }</Text>
+        <Row style={this.styles.container}>
           <Col>
-            <Text>Welcome to the info section</Text>
+            <Image 
+              source={{uri: `https://image.tmdb.org/t/p/w200${movie.poster_path}`}}
+              style={this.styles.imgThumbnail}
+            />
+          </Col>
+          <Col>
+            <Text style={this.styles.movieInfo}>
+              { movie.overview }
+            </Text>
           </Col>
         </Row>
       </Container>
