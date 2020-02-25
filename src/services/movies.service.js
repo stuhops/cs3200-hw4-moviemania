59ecc204336e745d9f6228efa28a50dd9b855eff
stuhops <stuart.hopkins@aggiemail.usu.edu>
2020-02-25 11:16:14 -0700

    Added API options

--- src/services/movies.service.js
+++ src/services/movies.service.js
@@ -5,22 +5,36 @@ const ROOT_URL = 'https://api.themoviedb.org/3';
 export default class MoviesService {
 
   static async search(searchTerm, pageNumber = 1) {
-    const result = await fetch(`${ROOT_URL}/search/movie?query=${searchTerm}&page=${pageNumber}`)
+    const result = await fetch(`${ROOT_URL}/search/movie?query=${searchTerm}&page=${pageNumber}`);
     const resJson = await result.json();
     return resJson.results;
   }
 
 
   static async getAllGenres() {
-    const result = await fetch(`${ROOT_URL}/genre/movie/list?${API_KEY}&language=en-US`)
+    const result = await fetch(`${ROOT_URL}/genre/movie/list?${API_KEY}&language=en-US`);
     const resJson = await result.json();
     return resJson.genres;
   }
 
 
   static async getMoviesByGenre(genreIds, pageNumber=1) {
-    const result = await fetch(`${ROOT_URL}/discover/movie?with_genres=${genreIds}&page=${pageNumber}&${API_KEY}&language=en-US`)
+    const result = await fetch(`${ROOT_URL}/discover/movie?with_genres=${genreIds}&page=${pageNumber}&${API_KEY}&language=en-US`);
     const resJson = await result.json();
     return resJson.results; 
   }
+
+
+  static async getMovieInfo(movieId) {
+    const result = await fetch(`${ROOT_URL}/movie/${movieId}?${API_KEY}&language=en-US`);
+    const resJson = await result.json();
+
+    const creditResult = await fetch(`${ROOT_URL}/movie/${movieId}/credits?${API_KEY}&language=en-US`);
+    const creditResJson = await creditResult.json();
+
+    resJson.results += {cast: creditResJson};
+
+    return resJson.results; 
+  }
+
 }
